9938c8c750363a4edfb5da27b6f130a70992c4e9
menady0 <menady20@gmail.com>
2025-05-01 13:10:23 +0300

    update function

--- Form1.cs
+++ Form1.cs
@@ -1,3 +1,7 @@
+using Newtonsoft.Json;
+using System.Net;
+using System.Reflection;
+
 namespace HelloWorld
 {
     public partial class Form1 : Form
@@ -5,6 +9,48 @@ namespace HelloWorld
         public Form1()
         {
             InitializeComponent();
+            CheckForUpdates();
+        }
+        public class UpdateInfo
+        {
+            public string version { get; set; }
+            public string url { get; set; }
+            public string changelog { get; set; }
+        }
+
+        private void CheckForUpdates()
+        {
+            string updateUrl = "https://raw.githubusercontent.com/menady0/HelloWorldApp/main/update-info.json";
+
+            try
+            {
+                using (WebClient client = new WebClient())
+                {
+                    string json = client.DownloadString(updateUrl);
+                    UpdateInfo update = JsonConvert.DeserializeObject<UpdateInfo>(json);
+
+                    Version currentVersion = Assembly.GetExecutingAssembly().GetName().Version;
+                    Version latestVersion = new Version(update.version);
+
+                    if (latestVersion > currentVersion)
+                    {
+                        DialogResult result = MessageBox.Show($"Update available!\n\n{update.changelog}\n\nDownload now?", "Update", MessageBoxButtons.YesNo);
+                        if (result == DialogResult.Yes)
+                        {
+                            System.Diagnostics.Process.Start(new System.Diagnostics.ProcessStartInfo
+                            {
+                                FileName = update.url,
+                                UseShellExecute = true
+                            });
+                            Application.Exit();
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Update check failed: " + ex.Message);
+            }
         }
     }
 }
